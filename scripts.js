1f454815f3161b8f05b70e90de610f075aae41bd
morgancostigan <morgan.costigan@gmail.com>
2020-04-17 19:28:01 -0500

    created paintToCanvas function, which currently finds the dimensions of the video

--- scripts.js
+++ scripts.js
@@ -14,7 +14,16 @@ function getVideo() {
         video.srcObject = localMediaStream; //createObjectURL is deprecated for chrome, so 'video.srcObject' is the fix i found
         // video.src = window.URL.createObjectURL(localMediaStream); //converts webcam stream to a URL format making it usable on this website 
         video.play();
+    })
+    .catch(err => {
+        console.error('oh snap!', err );
     });
 }//end getVideo function
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    console.log(width, height);
+}// end paintToCanvas function
 
 getVideo();
