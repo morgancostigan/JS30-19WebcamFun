f8f4e4ee99e67bcde57873f6798621bfb018cd9e
morgancostigan <morgan.costigan@gmail.com>
2020-04-17 19:09:57 -0500

    getVideo function to take video feed from webcam

--- scripts.js
+++ scripts.js
@@ -4,4 +4,14 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+console.log('kia ora');
 
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+        console.log(localMediaStream); 
+    });
+}//end getVideo function
+
+getVideo();
