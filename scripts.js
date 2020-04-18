bdc03c5f4de3aec9b9acb3b7f34b66a3d8bce38e
morgancostigan <morgan.costigan@gmail.com>
2020-04-17 19:18:23 -0500

    used video.srcObject assigned to localMediaStream to bring the webcam data into the browser

--- scripts.js
+++ scripts.js
@@ -11,6 +11,9 @@ function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
         console.log(localMediaStream); 
+        video.srcObject = localMediaStream; //createObjectURL is deprecated for chrome, so 'video.srcObject' is the fix i found
+        // video.src = window.URL.createObjectURL(localMediaStream); //converts webcam stream to a URL format making it usable on this website 
+        video.play();
     });
 }//end getVideo function
 
